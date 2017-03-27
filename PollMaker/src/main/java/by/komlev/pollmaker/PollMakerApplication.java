c83abb2ff588755b658e4153826b2dbdbbacd28a
ponipani <tokuchitoa77@gmail.com>
2017-03-27 22:23:17 +0300

    Initial commit

---
+++ PollMaker/src/main/java/by/komlev/pollmaker/PollMakerApplication.java
@@ -0,0 +1,13 @@
+package by.komlev.pollmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollMakerApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(PollMakerApplication.class, args);
+	}
+}
